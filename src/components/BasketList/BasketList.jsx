06a7ff9fa5da34d30af652cbc94408183ef51dfd
SkiPPy43 <skippy4343@yandex.ru>
2024-11-19 11:38:22 +0300

    fix

--- src/components/BasketList/BasketList.jsx
+++ src/components/BasketList/BasketList.jsx
@@ -19,7 +19,7 @@ function BasketList({
           />
         ))
       ) : (
-        <li className="collection-item">Корзина пуста</li>
+        <li className="collection-item">Корзина пуст</li>
       )}
       <li className="collection-item active">
         Общая стоимость :
